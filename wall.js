36d5259c1110486f434033a0056da21bd92a8ddd
Rookitmin <42980035+Rookitmin@users.noreply.github.com>
2019-01-24 13:01:07 -0500

    Update wall.js

--- wall.js
+++ wall.js
@@ -44,3 +44,48 @@ var createWall = function (x1, y1, x2, y2, bd) {
 		}
 	}
 }
+
+var createAttack = function (x, y, w, h, facing, speedX, speedY) {
+	this.pos = createVector(x,y);
+	this.posB = createVector(x + w, y + h);
+	this.size = createVector(w, h);
+	this.dir = facing;
+	this.speed = createVector(speedX, speedY);
+	
+	this.draw = function () {
+		noFill();
+		stroke("white");
+		strokeWeight(1);
+		rect(this.pos.x, this.pos.y, this.size.x, this.size.y);
+	}
+	
+	this.check = function () {
+		if (soul.check(this.pos.x, this.pos.y, this.posB.x, this.posB.y)) {
+			hp[0] --;
+		}
+	}
+	
+	this.change = function () {
+		this.pos.x += this.speed.x;
+		this.pos.y += this.speed.y;
+		this.posB.x = this.pos.x + this.size.x;
+		this.posB.y = this.pos.y + this.size.y;
+	}
+}
+var drawHP = function () {
+	fill("red");
+	noStroke();
+	rect(200, 310, 75, 20);
+	fill("yellow");
+	rect(200, 310, 75 * hp[0] / hp[1], 20);
+	fill("white");
+	textSize(12);
+	text("HP: " + hp[0] + " / " + hp[1], 285, 325);
+}
+
+
+
+
+
+
+
